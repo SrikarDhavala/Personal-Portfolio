7d3d9f60c144109f7c7e4f25bd3b9ddaadb77a51
SrikarDhavala <srikardhavala5305@gmail.com>
2026-04-16 12:51:15 +0530

    Added Particle Effect

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Projects from './sections/Projects'
 import Experience from './sections/Experience'
 import Contact from './sections/Contact'
 import Footer from './sections/Footer'
+import ParticlesBackground from './components/ParticlesBackground'
 
 export default function App(){
   return(
     <div className='relative gradient text-white'>
+      <ParticlesBackground/>
       <Navbar/>
       <Home/>
       <About/>
