6814174d777e184e91e14e1584006b45c97ebf0e
SrikarDhavala <srikardhavala5305@gmail.com>
2026-06-22 20:11:19 +0530

    Intro Animation & About section

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,29 @@ import Contact from './sections/Contact'
 import Footer from './sections/Footer'
 import ParticlesBackground from './components/ParticlesBackground'
 import CustomCursor from './components/CustomCursor'
+import { useState } from 'react'
+import IntroAnimation from './components/IntroAnimation'
 
-export default function App(){
-  return(
-    <div className='relative gradient text-white'>
-      <CustomCursor/>
-      {/* <ParticlesBackground/> */}
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Skills/>
-      <Projects/>
-      <Experience/>
-      <Contact/>
-      <Footer/>
-    </div>
+export default function App() {
+  const [introDone, setIntroDone] = useState(false);
+
+  return (
+    <>
+      {!introDone && <IntroAnimation onFinish={() => setIntroDone(true)} />}
+      {introDone && (
+        <div className='relative gradient text-white'>
+          <CustomCursor />
+          {/* <ParticlesBackground/> */}
+          <Navbar />
+          <Home />
+          <About />
+          <Skills />
+          <Projects />
+          <Experience />
+          <Contact />
+          <Footer />
+        </div>
+      )}
+    </>
   )
 }
